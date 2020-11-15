49feb674c59cbdf07d7d1de88a5a94e6fe5a3018
Jintin <jintin.lin1018@gmail.com>
2020-11-15 10:41:29 +0800

    Add flow support

--- app/src/main/java/com/jintin/fancylocation/MainActivity.kt
+++ app/src/main/java/com/jintin/fancylocation/MainActivity.kt
@@ -5,17 +5,26 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.lifecycle.lifecycleScope
 import com.jintin.fancylocation.livedata.LocationData
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         const val LOCATION_REQUEST = 22
+
+        const val TYPE_LIVEDATA = 0
+        const val TYPE_FLOW = 1
     }
 
     private val mainViewModel by viewModels<MainViewModel>()
+
+    private val type = TYPE_FLOW
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,11 +51,35 @@ class MainActivity : AppCompatActivity() {
             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST)
             return
         }
-        mainViewModel.locationLiveData.observe(this) {
-            findViewById<TextView>(R.id.location).text = when (it) {
-                is LocationData.Success -> it.location.toString()
-                is LocationData.Fail -> "Fail to get location"
+
+        when (type) {
+            TYPE_LIVEDATA -> liveDataObserve()
+            TYPE_FLOW -> flowObserve()
+        }
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+    private fun flowObserve() {
+        lifecycleScope.launch {
+
+            @Suppress("EXPERIMENTAL_API_USAGE")
+            mainViewModel.locationFlow.get().collect {
+                updateUI(it)
             }
+        }
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+    private fun liveDataObserve() {
+        mainViewModel.locationLiveData.observe(this) {
+            updateUI(it)
+        }
+    }
+
+    private fun updateUI(locationData: LocationData) {
+        findViewById<TextView>(R.id.location).text = when (locationData) {
+            is LocationData.Success -> locationData.location.toString()
+            is LocationData.Fail -> "Fail to get location"
         }
     }
 
---
+++ lib/src/main/java/com/jintin/fancylocation/livedata/LocationLiveData.kt
@@ -0,0 +1,78 @@
+package com.jintin.fancylocation.livedata
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Looper
+import androidx.annotation.RequiresPermission
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.*
+
+class LocationLiveData(
+    private val context: Context,
+    private val locationRequest: LocationRequest
+) : LiveData<LocationData>() {
+
+    private val locationProvider by lazy {
+        LocationServices.getFusedLocationProviderClient(context)
+    }
+
+    private val locationListener by lazy {
+        LocationListener()
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+    override fun onActive() {
+        super.onActive()
+        requestLocationUpdates()
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        removeLocationUpdates()
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+    override fun observe(
+        owner: LifecycleOwner,
+        observer: Observer<in LocationData>
+    ) {
+        super.observe(owner, observer)
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+    override fun observeForever(observer: Observer<in LocationData>) {
+        super.observeForever(observer)
+    }
+
+    private fun removeLocationUpdates() {
+        locationProvider.removeLocationUpdates(locationListener)
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+    private fun requestLocationUpdates() {
+        locationProvider.requestLocationUpdates(
+            locationRequest,
+            locationListener,
+            Looper.getMainLooper()
+        )
+    }
+
+    inner class LocationListener : LocationCallback() {
+        override fun onLocationResult(result: LocationResult?) {
+            result?.lastLocation?.let {
+                value = LocationData.Success(it)
+            }
+        }
+
+        @SuppressLint("MissingPermission")
+        override fun onLocationAvailability(availability: LocationAvailability?) {
+            if (availability?.isLocationAvailable == false) {
+                value = LocationData.Fail
+            }
+        }
+    }
+
+}
