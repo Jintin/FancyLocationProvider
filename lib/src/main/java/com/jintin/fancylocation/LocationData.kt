be352757c4ce8e903e1656a4ed13f29012e957f2
Jintin <jintin.lin1018@gmail.com>
2023-11-01 15:28:26 +0800

    Upgrade deps (#15)

--- lib/src/main/java/com/jintin/fancylocation/LocationData.kt
+++ lib/src/main/java/com/jintin/fancylocation/LocationData.kt
@@ -3,8 +3,8 @@ package com.jintin.fancylocation
 import android.location.Location
 
 sealed class LocationData {
-    object Init : LocationData()
-    object Loading : LocationData()
+    data object Init : LocationData()
+    data object Loading : LocationData()
     data class Success(val location: Location) : LocationData()
-    object Fail : LocationData()
+    data object Fail : LocationData()
 }
